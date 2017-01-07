53b2be1a62c2106bcb9989b1fb17fa10a2fb9792
Elana Kopelevich <ekopelevich@gmail.com>
2017-01-06 18:11:02 -0700

    refactor with arrays and switch statement

--- src/com/elana/calcengine/Main.java
+++ src/com/elana/calcengine/Main.java
@@ -1,25 +1,35 @@
 package com.elana.calcengine;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        double val1 = 200.0d;
-        double val2 = 50.0d;
-        double result;
-        char opCode = 'a';
+        double[] leftVals = {200.0d, 40.0d, 320.0d, 50.0d};
+        double[] rightVals = {50.0d, 120.0d, 10.0d, 90.0d};
+        char[] opCodes = {'a', 's', 'd', 'm'};
+        double[] results = new double[opCodes.length];
 
-        if (opCode == 'a')
-            result = val1 + val2;
-        else if (opCode == 's')
-            result = val1 - val2;
-        else if (opCode == 'm')
-            result = val1 * val2;
-        else if (opCode == 'd')
-            result = val2 != 0.0d ? val1 / val2 : 0.0d;
-        else {
-            System.out.println("Error - invalid opCode");
-            result = 0.0d;
+        for (int i = 0; i < opCodes.length; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    results[i] = leftVals[i] + rightVals[i];
+                    break;
+                case 's':
+                    results[i] = leftVals[i] - rightVals[i];
+                    break;
+                case 'd':
+                    results[i] = (rightVals[i] != 0.0d) ? (leftVals[i] / rightVals[i]) : 0.0d;
+                    break;
+                case 'm':
+                    results[i] = leftVals[i] * rightVals[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    results[i] = 0.0d;
+                    break;
+            }
         }
-        System.out.println(result);
+        System.out.println(Arrays.toString(results));
     }
 }
